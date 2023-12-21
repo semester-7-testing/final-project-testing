af3fbff452ce97a7f738e2ae289896a4c649ff38
Šimon Bučko <sibu@queue-it.com>
2023-12-21 21:26:22 +0100

    remove console logs

--- server/routes/auth/authRouter.integration.spec.js
+++ server/routes/auth/authRouter.integration.spec.js
@@ -1,23 +1,23 @@
-import User from '../../models/user';
-import request from 'supertest';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
+import User from "../../models/user";
+import request from "supertest";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 dotenv.config();
 
 const appUrl = `http://localhost:${process.env.SERVER_PORT}`;
 
 const testUser = {
-  name: 'test',
-  email: 'test@example.com',
-  password: 'test1234',
+  name: "test",
+  email: "test@example.com",
+  password: "test1234",
   isAdmin: false,
 };
 
 const newUser = {
-  name: 'newUser1',
-  email: 'newUser@example.com',
-  password: 'test1234',
+  name: "newUser1",
+  email: "newUser@example.com",
+  password: "test1234",
   isAdmin: false,
 };
 
@@ -27,7 +27,7 @@ async function seedDatabase() {
 }
 
 beforeAll(async () => {
-  mongoose.set('strictQuery', true).connect(process.env.MONGO_URI);
+  mongoose.set("strictQuery", true).connect(process.env.MONGO_URI);
 });
 
 beforeEach(async () => {
@@ -42,78 +42,77 @@ afterAll(async () => {
   mongoose.connection.close();
 });
 
-describe('Auth router', () => {
-  it('on signup it should return the error if user has been found', async () => {
+describe("Auth router", () => {
+  it("on signup it should return the error if user has been found", async () => {
     const response = await request(appUrl)
-      .post('/api/auth/signup')
+      .post("/api/auth/signup")
       .send(testUser)
-      .set('Content-Type', 'application/json')
-      .set('Accept', 'application/json');
+      .set("Content-Type", "application/json")
+      .set("Accept", "application/json");
 
     expect(response.statusCode).toBe(400);
-    expect(response.body.errors[0].msg).toBe('Email already in use');
+    expect(response.body.errors[0].msg).toBe("Email already in use");
   });
 
-  it('on signup should create a new user in the database', async () => {
+  it("on signup should create a new user in the database", async () => {
     const response = await request(appUrl)
-      .post('/api/auth/signup')
+      .post("/api/auth/signup")
       .send(newUser)
-      .set('Content-Type', 'application/json')
-      .set('Accept', 'application/json');
+      .set("Content-Type", "application/json")
+      .set("Accept", "application/json");
 
     const addedUser = await User.findOne({ email: newUser.email });
     expect(response.statusCode).toBe(201);
     expect(addedUser).not.toBeNull();
   });
 
-  it('on login it should return the error if user has not been found', async () => {
+  it("on login it should return the error if user has not been found", async () => {
     const user = {
       email: testUser.email,
-      password: 'wrongPassword',
+      password: "wrongPassword",
     };
 
     const response = await request(appUrl)
-      .post('/api/auth/login')
+      .post("/api/auth/login")
       .send(user)
-      .set('Content-Type', 'application/json')
-      .set('Accept', 'application/json');
+      .set("Content-Type", "application/json")
+      .set("Accept", "application/json");
 
     expect(response.statusCode).toBe(400);
-    expect(response.body.errors[0].msg).toBe('Invalid credentials');
+    expect(response.body.errors[0].msg).toBe("Invalid credentials");
   });
 
-  it('on login it should return the error if credentials do not match', async () => {
+  it("on login it should return the error if credentials do not match", async () => {
     const response = await request(appUrl)
-      .post('/api/auth/login')
+      .post("/api/auth/login")
       .send(newUser)
-      .set('Content-Type', 'application/json')
-      .set('Accept', 'application/json');
+      .set("Content-Type", "application/json")
+      .set("Accept", "application/json");
 
     expect(response.statusCode).toBe(400);
-    expect(response.body.errors[0].msg).toBe('Invalid credentials');
+    expect(response.body.errors[0].msg).toBe("Invalid credentials");
   });
 
-  it('on login it should return status 200 if the user log in was successful', async () => {
+  it("on login it should return status 200 if the user log in was successful", async () => {
     const testLoginUser = {
-      email: 'testLoginUser@gmail.com',
-      password: 'testLoginUser',
-      name: 'loginUser',
-      isAdmin: 'false',
+      email: "testLoginUser@gmail.com",
+      password: "testLoginUser",
+      name: "loginUser",
+      isAdmin: "false",
     };
 
     await request(appUrl)
-      .post('/api/auth/signup')
+      .post("/api/auth/signup")
       .send(testLoginUser)
-      .set('Content-Type', 'application/json')
-      .set('Accept', 'application/json');
+      .set("Content-Type", "application/json")
+      .set("Accept", "application/json");
 
     const response = await request(appUrl)
-      .post('/api/auth/login')
+      .post("/api/auth/login")
       .send(testLoginUser)
-      .set('Content-Type', 'application/json')
-      .set('Accept', 'application/json');
+      .set("Content-Type", "application/json")
+      .set("Accept", "application/json");
 
-    console.log(response.body);
     expect(response.statusCode).toBe(200);
   });
 });
