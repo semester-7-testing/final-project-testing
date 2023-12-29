1b390034039c07c19e02e0d4def69dee43f597f5
Lars Riel <larsriel@gmail.com>
2023-12-29 04:52:06 +0100

    updated k6 configs

--- k6/run.sh
+++ k6/run.sh
@@ -1,13 +1,6 @@
-# Headless
-# docker run --rm -i \
-#     -v $(pwd)/output:/output \
-#     grafana/k6 run - <scripts/script.js --out json=/output/test.json
-
-# Linux
 docker run -it --rm \
     -v $(pwd):/scripts \
+    -v $(pwd)/tmp:/work \
     -p 5665:5665 \
-    ghcr.io/grafana/xk6-dashboard:latest run --out=dashboard /scripts/script.js
-
-# Windows
-# docker run -v %cd%:/scripts -p 5665:5665 -it --rm ghcr.io/grafana/xk6-dashboard:latest run --out=dashboard /scripts/script.js
+    ghcr.io/grafana/xk6-dashboard:latest \
+    run --out=dashboard=report=/work/report.html /scripts/script.js
--- k6/script.js
+++ k6/script.js
@@ -1,26 +1,31 @@
 import http from 'k6/http';
 import { sleep } from 'k6';
-import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
 
 export const options = {
+    // normal
     stages: [
-        { duration: '1m', target: 100 }, // traffic ramp-up from 1 to 100 users over 1 minute.
-        { duration: '5m', target: 100 }, // stay at 100 users for 5 minutes
-        { duration: '1m', target: 200 }, // traffic ramp-up from 100 to 200 users over 1 minute (peak hour starts)
-        { duration: '5m', target: 200 }, // stay at 200 users for 5 minutes
-        { duration: '1m', target: 100 }, // traffic ramp-down from 200 to 100 users over 1 minute (peak hour ends)
-        { duration: '5m', target: 100 }, // stay at 100 users for 5 minutes
-        { duration: '1s', target: 0 }, // ramp-down to 0 users
+        { duration: '1m', target: 500 }, // traffic ramp-up from 1 to 100 users over 1 minute.
+        { duration: '2m', target: 500 }, // stay at 100 users for 5 minutes
+        { duration: '1m', target: 1500 }, // traffic ramp-up from 100 to 200 users over 1 minute (peak hour starts)
+        { duration: '2m', target: 1500 }, // stay at 200 users for 5 minutes
+        { duration: '1m', target: 500 }, // traffic ramp-down from 200 to 100 users over 1 minute (peak hour ends)
+        { duration: '2m', target: 500 }, // stay at 100 users for 5 minutes
+        { duration: '1m', target: 0 }, // ramp-down to 0 users
       ],
+    // extreme
+    // stages: [
+    //     { duration: '1m', target: 2000 }, // traffic ramp-up from 1 to 100 users over 1 minute.
+    //     { duration: '2m', target: 2000 }, // stay at 100 users for 5 minutes
+    //     { duration: '1m', target: 4000 }, // traffic ramp-up from 100 to 200 users over 1 minute (peak hour starts)
+    //     { duration: '2m', target: 4000 }, // stay at 200 users for 5 minutes
+    //     { duration: '1m', target: 2000 }, // traffic ramp-down from 200 to 100 users over 1 minute (peak hour ends)
+    //     { duration: '2m', target: 2000 }, // stay at 100 users for 5 minutes
+    //     { duration: '1m', target: 0 }, // ramp-down to 0 users
+    //   ],
 };
 
 export default function () {
-  http.get('https://kea-foot-shop.onrender.com/');
+  // http.get('http://192.168.122.117:3000/');
+  http.get('http://192.168.122.117:8080/api/products');
   sleep(1);
 }
-
-export function handleSummary(data) {
-    return {
-      "/output/summary.html": htmlReport(data),
-    };
-  }
